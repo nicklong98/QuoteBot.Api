af665be2d5c08d8f9bdc8a9d9b291d2b086fcc62
nicklong98 <work@singularity-id.com>
2020-12-04 12:11:26 -0500

    get specific users quotes

--- QuoteBot.Api/Quotes/GetQuotes.cs
+++ QuoteBot.Api/Quotes/GetQuotes.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
@@ -34,6 +35,21 @@ namespace QuoteBot.Api.Quotes
             var quote = _context.Quotes.Where(x => x.ServerId == serverId)
                 .ProjectTo<GetQuoteResponseVm>(_mapper.ConfigurationProvider).ToList().OrderBy(__ => Guid.NewGuid())
                 .FirstOrDefault();
+            return Ok(quote);
+        }
+
+        [HttpGet("{serverId}/random/{userId}")]
+        public ActionResult<GetQuoteResponseVm> GetRandomQuoteForUserOnServer(long serverId, long userId)
+        {
+            var quote = _context.Quotes.Where(x => x.ServerId == serverId && x.UserId == userId)
+                .ProjectTo<GetQuoteResponseVm>(_mapper.ConfigurationProvider).ToList()
+                .OrderBy(__ => Guid.NewGuid()).FirstOrDefault();
+            if (quote == default)
+            {
+                quote = new GetQuoteResponseVm("Look I'm lame and don't have any quotes in quote bot ok?",
+                    userId.ToString(), null);
+            }
+
             return Ok(quote);
         }
     }
